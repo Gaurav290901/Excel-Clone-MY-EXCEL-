57552ec37c52ca0da0561f866762e64937d3f66a
Gaurav290901 <gaurav.gayan01@gmail.com>
2022-05-09 01:02:09 +0530

    Two way winding added

--- grid.js
+++ grid.js
@@ -12,14 +12,12 @@ for(let i = 0; i < rows; i++) {
     addressCol.innerText = i + 1;
     addressColCont.appendChild(addressCol);
 }
-
 for (let i = 0; i < cols; i++) {
     let addressRow = document.createElement("div");
     addressRow.setAttribute("class", "address-row");
     addressRow.innerText = String.fromCharCode(65 + i);
     addressRowCont.appendChild(addressRow);  
 }
-
 for(let i = 0; i < rows; i++) {
     let rowCont = document.createElement("div");
     rowCont.setAttribute("class", "row-cont");
@@ -27,18 +25,24 @@ for(let i = 0; i < rows; i++) {
         let cell = document.createElement("div");
         cell.setAttribute("class", "cell");
         cell.setAttribute("contenteditable", "true");
+        cell.setAttribute("spellcheck", "false");
+        //atribute for cell and storage identification
+        cell.setAttribute("rid",i);
+        cell.setAttribute("cid",j);
         rowCont.appendChild(cell);
         addListenerForAddressBarDisplay(cell, i, j);
     }
     cellsCont.appendChild(rowCont);
-
 }
 
 function addListenerForAddressBarDisplay(cell, i, j){
     cell.addEventListener("click", (e) =>{
-        let rowID = i+1;
+        let rowID = i + 1;
         let colID = String.fromCharCode(65 + j);
         addressBar.value = `${colID}${rowID}`;
-
     })
 }
+
+//by default click on first cell via dom
+let firstCell = document.querySelector(".cell");
+firstCell.click();
